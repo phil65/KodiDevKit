ec79e22d718101ec2eeeb01a6b41b063e4997685
phil65 <philipptemminghoff@gmx.de>
2016-05-12 04:40:16 +0200

    include work

--- libs/include.py
+++ libs/include.py
@@ -3,14 +3,20 @@
 # Copyright (C) 2015 - Philipp Temminghoff <phil65@kodi.tv>
 # This program is Free Software see LICENSE file for details
 
+from lxml import etree as ET
+import os
+
 
 class Include(dict):
 
     def __init__(self, *args, **kwargs):
         super(Include, self).__init__(*args, **kwargs)
         self.node = kwargs.get("node")
-        self.filename = kwargs.get("filename")
         self.file = kwargs.get("file")
+        if self.node.getnext() is not None:
+            self.length = self.node.getnext().sourceline - self.node.sourceline
+        else:
+            self.length = None
 
     def __getitem__(self, key):
         if key == "line":
@@ -23,7 +29,14 @@ class Include(dict):
             return self.filename
         if key == "file":
             return self.file
+        if key == "content":
+            return ET.tostring(self.node, pretty_print=True, encoding="unicode")
+        if key == "length":
+            return self.length
         super(Include, self).__getitem__(key)
+
+    def get(self, key):
+        return self.__getitem__(key)
 
     @property
     def line(self):
@@ -34,13 +47,14 @@ class Include(dict):
         return self.node.tag
 
     @property
-    def name(self):
+    def content(self):
         return self.node.text
 
     @property
-    def filename(self):
-        return self.filename
+    def name(self):
+        return self.node.attrib.get("name")
 
     @property
-    def file(self):
-        return self.file
+    def filename(self):
+        return os.path.basename(self.file)
+
