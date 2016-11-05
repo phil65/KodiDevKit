7d1b2d15bcd304cd8219a5b7681945792dc868fa
phil65 <philipptemminghoff@gmx.de>
2016-11-05 03:45:39 +0100

    add offsetx/y to double tags check

--- libs/infoprovider.py
+++ libs/infoprovider.py
@@ -143,7 +143,8 @@ POS_TAGS = {"posx", "posy", "left", "right", "top", "bottom", "centerleft", "cen
 # TODO: special cases: label for fadelabel
 DOUBLE_TAGS = {"camera", "posx", "posy", "top", "bottom", "left", "right", "centertop", "centerbottom", "centerleft", "centerright", "width", "height",
                "colordiffuse", "texturefocus", "texturenofocus", "font", "selected", "textcolor", "disabledcolor", "selectedcolor", "usecontrolcoords",
-               "shadowcolor", "align", "aligny", "textoffsetx", "textoffsety", "pulseonselect", "textwidth", "focusedcolor", "invalidcolor", "angle", "hitrect", "orientation"}
+               "shadowcolor", "align", "aligny", "textoffsetx", "textoffsety", "pulseonselect", "textwidth", "focusedcolor", "invalidcolor", "angle", "hitrect", "orientation",
+               "offsetx", "offsety"}
 # check that some nodes only contain specific text
 # check that some attributes may only contain specific values
 ALLOWED_VALUES = {"align": {"left", "center", "right", "justify"},
