b63c4f055c7d536d568982f8451e97046dca1b6e
phil65 <philipptemminghoff@googlemail.com>
2017-06-19 12:47:58 +0200

    tests.py

---
+++ tests.py
@@ -0,0 +1,11 @@
+# -*- coding: utf8 -*-
+
+# Copyright (C) 2017 - Philipp Temminghoff <phil65@kodi.tv>
+# This program is Free Software see LICENSE file for details
+
+import os
+import sys
+import codecs
+import logging
+
+exit(0)
